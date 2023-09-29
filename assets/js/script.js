bf040dbd8e32fef558c4b020a05d558eb93d1efe
pcrainer38 <pammie_78704@yahoo.com>
2023-09-29 18:30:55 -0500

    updates to html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
-var typingInput = document.querySelector(".typing.input");
+// var typingInput = document.querySelector(".typing-input");
 var startBtnInput = document.querySelector("#startBtn");
 
-var words = ["javascript", "css"];
+
+var words = ["javascript", "css", "html", "function"];
 var randWords = Math.floor(Math.random() * words.length)
+var selectedWord = words[randWords];
 var guessedLetters = [];
 
+console.log(selectedWord);
+
+// Start Button even listener
 startBtnInput.addEventListener("click", function(event) {
     event.preventDefault();
-
 })
 
+// To gather input when user presses letter key on keyboard
+function keydownAction(event) {
+    var keyDown = event.keyDown;
+    
+    document.querySelector("#key").textContent = keyDown;
+}
 
+// Will display dashes for selected word
+var displayWord = "";
+for (var i = 0; i < selectedWord.length; i++) {
+    displayWord += "_";
+}
+document.getElementById("displayWord").textContent = displayWord;
+
+
+var playGame = function (){
+   
+    // var inputElement = document.getElementById(typingInput);
+
+
+}    
 
