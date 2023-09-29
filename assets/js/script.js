1bbb0670d167ae67b0ee73e6ed81ff98331c468b
pcrainer38 <pammie_78704@yahoo.com>
2023-09-29 15:58:08 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var typingInput = document.querySelector(".typing.input");
+var startBtnInput = document.querySelector("#startBtn");
+
+var words = ["javascript", "css"];
+var randWords = Math.floor(Math.random() * words.length)
+var guessedLetters = [];
+
+startBtnInput.addEventListener("click", function(event) {
+    event.preventDefault();
+
+})
+
+
+
